4983f478b2b9ff0a3ec32dce2d940787dba9aac5
Kenneth Mak <kennmak@gmail.com>
2018-04-08 21:15:06 -0400

    Revert "Revert "Merge branch 'master' into Beta_Li""
    This reverts commit 53a57bae004a88887d63881beb6af56fbb108cb8.

---
+++ Source/Liberators/TestChar.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "TestChar.generated.h"
+
+UCLASS()
+class LIBERATORS_API ATestChar : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ATestChar();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason);
+	FDelegateHandle OnControllerConnectionHandle;
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Input")
+		void OnControllerConnectionChange(bool Connected, int32 UserID, int32 ControllerID);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Input")
+		FString GetPlayerName();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+
+	
+	
+};
